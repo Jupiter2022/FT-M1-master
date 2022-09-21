b25da61def51f04baf73073efd631108793cca45
Júìter2022 <jupiter2021.2022@gmail.com>
2022-09-21 13:49:27 -0500

    HomeWork tuesday finish full

--- 05-EstructuraDeDatos-II/homework/pruebas.js
+++ 05-EstructuraDeDatos-II/homework/pruebas.js
@@ -1,62 +1,24 @@
-function BinarySearchTree(value) {
-  this.value = value;
-  this.left = null;
-  this.right = null;
-}
-
-
-BinarySearchTree.prototype.size = function(){
-  if(!this.value) return null;
-  if(!this.left && !this.right)return 1;
-  if(this.left && !this.right)return 1 + this.left.size();
-  if(!this.left && this.right)return 1 + this.right.size();  
-  if(this.left && this.right)return 1 + this.left.size() + this.right.size();
-}
-BinarySearchTree.prototype.insert = function(value){ 
-  if(!this.value) this.value = value;
-
-  if (value > this.value){
-    if (!this.right){
-      this.right = new BinarySearchTree(value);
-    }else{
-      this.right.insert(value);
+function selectionSort(array) {
+  // Implementar el método conocido como selectionSort para ordenar de menor a mayor
+  // el array recibido como parámetro utilizando dos arreglos
+  // Devolver el array ordenado resultante
+  // Tu código:
+  let cont = [];
+  for(let i = 0; i < array.length; i++){
+    let menor = array[i];
+    for(var j = i+1;j < array.length; j++){
+      if(menor > array[j]){
+        array[i]= array[j]
+        array[j] = menor; 
+        menor = array[i];
+      }
     }
-  }else{
-    if (!this.left){
-      this.left = new BinarySearchTree(value);
-    }else{
-      this.left.insert(value)
-    }
+    console.log(menor);
+    cont[i] = menor
+
   }
 
+  return cont;
 }
 
-
-
-
-
-BinarySearchTree.prototype.contains = function(value){
-  //contains: retorna true o false luego de evaluar si cierto valor existe dentro del árbol
-  if (value === this.value) return true;
-  if(value > this.value){
-    if(!this.right) return false;
-    else return this.right.contains(value);
-  }else{
-    if(!this.left) return false;
-    else return this.left.contains(value);
-  }
-
-}
-BinarySearchTree.prototype.depthFirstForEach = function(){}
-BinarySearchTree.prototype.breadthFirstForEach = function(){}
-
-let arbol = new BinarySearchTree(20)
-arbol.insert(32);
-arbol.insert(30);
-arbol.insert(35);
-arbol.insert(12);
-arbol.insert(15);
-arbol.insert(11);
-console.log(arbol.contains(11))
-console.log(arbol.size())
-console.log(arbol)
+console.log(selectionSort([10, -2, -7, 4]))
--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,15 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let primos = [2,3,5,7,11,13,17]
+  let array = [1];
+  for (let i = 0; i < primos.length; i++){
+    while(num % primos[i] === 0){
+      num = num / primos[i];
+      array.push(primos[i])
+    }
+  }
+  return array;
 }
 
 function bubbleSort(array) {
@@ -14,7 +22,17 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  var order = array;
+  for(var i = 0; i < order.length; i++){
+    for(var j = 0; j < order.length - i; j++){
+      if (order[j] > order[j+1]){ 
+        let aux = order[j];
+        order[j] = order[j+1];
+        order[j+1] = aux;
+      }
+    }
+  }
+  return order;
 }
 
 
@@ -23,7 +41,17 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  let order = array;
+  for(let i = 1; i < order.length; i++){
+     let j = i - 1;
+     let temp = order[i];
+     while(j>=0 && order[j] > temp){
+       order[j+1] = order[j];
+       j--;
+     }
+     order[j+1] = temp;
+  }
+  return order;
 }
 
 
@@ -32,6 +60,22 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  let cont = [];
+  for(let i = 0; i < array.length; i++){
+    let menor = array[i];
+    for(var j = i+1;j < array.length; j++){
+      if(menor > array[j]){
+        array[i]= array[j]
+        array[j] = menor; 
+        menor = array[i];
+      }
+    }
+    console.log(menor);
+    cont[i] = menor
+
+  }
+
+  return cont;
 
 }
 
